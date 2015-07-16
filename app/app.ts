f5814d00602700358b9b15e0dc214d6541400bbd
Michael Lehmann <leh@zuehlke.com>
2015-07-16 12:51:47 +0200

    Bootstrap the standard Angular2 Quickstart app

---
+++ app/app.ts
@@ -0,0 +1,11 @@
+///<reference path="../typings/angular2/angular2.d.ts"/>
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({
+  selector: 'my-app'
+})
+@View({
+  template: '<h1>My first Angular 2 App</h1>'
+})
+export class AppComponent {
+}
---
+++ main.ts
@@ -0,0 +1,6 @@
+///<reference path="./typings/angular2/angular2.d.ts"/>
+
+import {bootstrap} from 'angular2/angular2';
+import {AppComponent} from 'app/app';
+
+bootstrap(AppComponent);
