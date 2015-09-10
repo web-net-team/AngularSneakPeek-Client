aa0f929030300d872e098a3874c497ec00ae6f23
Michael Lehmann <lehmamic@bluewin.ch>
2015-09-10 11:45:08 +0200

    feat(presentation): Initial project struction with hello world

--- components/app/app.ts
+++ components/app/app.ts
@@ -1,35 +1,20 @@
 ///<reference path="../../typings/angular2/angular2.d.ts"/>
 
-import {Component, View, bootstrap} from 'angular2/angular2';
-import {Navbar} from '../navbar/navbar';
-import {Board} from '../board/board';
+import {Component, View} from 'angular2/angular2';
 
 @Component({
   selector: 'kanban-app'
 })
 @View({
-  styles: [`
-    navbar { 
-      position: fixed;
-      top: 0;
-      left: 0;
-      right: 0;
-      height: 50px;
-    }
-    board {
-      position: fixed;
-      top: 50px;
-      bottom: 0;
-      left: 0;
-      right: 0;
-    }
-  `],
+  styles: [],
   template:
-  `<div>
-    <navbar></navbar>
-    <board></board>
-  </div>`,
-  directives: [Navbar, Board]
+  `Hello {{ name }}`,
+  directives: []
 })
 export class KanbanApp {
+  name: string;
+  
+  constructor() {
+    this.name = 'Alice';
+  }
 }
